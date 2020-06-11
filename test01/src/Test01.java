7678dd6449946196df010eb15fae317abb8ce964
jshanxuerui <764795838@qq.com>
2020-06-11 11:40:38 +0800

    啊

--- test01/src/Test01.java
+++ test01/src/Test01.java
@@ -6,9 +6,9 @@
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("dev");
-        System.out.println(111);
-        System.out.println(222);
-        System.out.println(333);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
         System.out.println(555);
         System.out.println(555);
         System.out.println(777);
