f8833c33d59bd671152ff7dc6819743ae6d58cd6
jshanxuerui <764795838@qq.com>
2020-06-11 13:55:03 +0800

    啊

--- test01/src/Test01.java
+++ test01/src/Test01.java
@@ -5,7 +5,7 @@
  */
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("de");
+        System.out.println("000");
         System.out.println(111);
         System.out.println(222);
         System.out.println(33);
