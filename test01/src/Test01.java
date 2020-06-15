5c68c94ad70631a68dc15fc868b89cbb54e458ed
jshanxuerui <764795838@qq.com>
2020-06-15 09:09:22 +0800

    啊

--- test01/src/Test01.java
+++ test01/src/Test01.java
@@ -5,7 +5,8 @@
  */
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println(222);
-        System.out.println(444);
+        System.out.println(333);
+        System.out.println(555);
+
     }
 }
